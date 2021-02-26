b210c68f5f10004cbba03c44037c0c19cbda9b40
Obanda <emmanuelwakhisi@gmail.com>
2021-02-26 14:34:35 +0300

    first commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,33 @@
 require('./bootstrap');
+
+// Import modules...
+import { createApp, h } from 'vue';
+import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+import { vue } from 'laravel-mix';
+import { VueLoaderPlugin } from 'vue-loader';
+
+const Swal = require('sweetalert2').default
+
+window.Toast = Swal.mixin({
+    toast: true,
+    position: 'top-right',
+    timer: 3500,
+    timerProgressBar: true
+})
+
+
+const app = document.getElementById('app');
+
+createApp({
+    render: () =>
+        h(InertiaApp, {
+            initialPage: JSON.parse(el.dataset.page),
+            resolveComponent: (name) => require(`./Pages/${name}`).default,
+        }),
+})
+    .mixin({ methods: { route } })
+    .use(InertiaPlugin)
+    .mount(el);
+
+InertiaProgress.init({ color: '#4B5563' });
